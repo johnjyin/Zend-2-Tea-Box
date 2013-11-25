8f8c8b0032223bb02ea562f46dd54754dd785c65
John Yin <john.yin@ebizdesigner.com>
2013-11-26 10:02:18 +1100

    Revert "Initial"
    This reverts commit 0797d631636fee7db8234cb29daca0ad2459004c.

--- module/Tea/autoload_classmap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
-   As we are in development, we don¡¯t need to load 
-   files via the classmap, so we provide an empty 
-   array for the classmap autoloader. 
- **/
-return array();
-?>
